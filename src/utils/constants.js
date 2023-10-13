4bbfa5d8091b47c91865b7816c723d42068bbc58
SunDeveloper1 <harikumar01947@gmail.com>
2023-10-13 23:40:05 +0530

    proxy 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const LOGO_URL =
   "https://img.freepik.com/premium-vector/burger-logo-vector-art-design_260747-248.jpg?w=740";
 
 export const MENU_URL=
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.9124336&lng=75.7872709&restaurantId="  
+"https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.9124336&lng=75.7872709&restaurantId="  
 
 export const IMG_URL=
 "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
